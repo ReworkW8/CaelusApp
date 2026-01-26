ea007715b7924535114992d2b883d30440515940
Rework <reworkw8official@gmail.com>
2026-01-26 14:25:17 +0100

    server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 
 app.get('https://www.caelus.lol/status', (req, res) => {
-    res.send("version1.0=true;tileData=Test");
+    res.send("version1.0=true;tileData=Test!");
 });
 
 
